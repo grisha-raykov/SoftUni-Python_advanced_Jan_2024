ccfa8cc9754a775a35d95b5aa067442a2b2deb4b
abruski <abruski@gmail.com>
2024-02-13 16:11:04 +0200

    Add labs and exercises

--- 03. Lists as stacks and queues/05. Hot potato.py
+++ 03. Lists as stacks and queues/05. Hot potato.py
@@ -1,7 +1,10 @@
 from collections import deque
 
-names = input().split()
-n_toss = int(input())
+names = deque(input().split())
+n_toss = int(input()) - 1
 
 while len(names) > 1:
-    pass
+    names.rotate(-n_toss)
+    removed_kid = names.popleft()
+    print(f'Removed {removed_kid}')
+print(f'Last is {names.pop()}')
