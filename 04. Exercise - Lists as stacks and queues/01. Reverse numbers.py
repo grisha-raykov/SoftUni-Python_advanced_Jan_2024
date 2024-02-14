11722ca0acaef608c59790d24572e8b78ec9bb36
abruski <abruski@gmail.com>
2024-02-14 09:26:04 +0200

    add exercises

--- 04. Exercise - Lists as stacks and queues/01. Reverse numbers.py
+++ 04. Exercise - Lists as stacks and queues/01. Reverse numbers.py
@@ -1,1 +1,7 @@
-numbers = input().split()
+from collections import deque
+
+numbers = deque(input().split())
+
+# Works with while too
+for _ in range(len(numbers)):
+    print(numbers.pop(), end=' ')
