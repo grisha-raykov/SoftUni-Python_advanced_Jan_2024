e225781f1a0fa3c0cc4feb8c4a9271fbf8dd7c0f
abruski <abruski@gmail.com>
2024-04-08 10:57:20 +0300

    Change somethings

--- 04. Exercise - Lists as stacks and queues/02. Stacked Queries.py
+++ 04. Exercise - Lists as stacks and queues/02. Stacked Queries.py
@@ -7,14 +7,14 @@ for _ in range(int(input())):
     if command == 1:
         numbers.append(numbers_data[1])
     elif command == 2:
-        if numbers: # Check if the stack is empty
+        # Check if the stack is not empty. If its empty and we apply pop() it will produce an error
+        if numbers:
             numbers.pop()
     elif command == 3:
         if numbers:
             print(max(numbers))
     elif command == 4:
-        if numbers:
-            print(min(numbers))
+        print(min(numbers))
 
 numbers.reverse()
-print(*numbers, sep=', ')
+print(*numbers)
